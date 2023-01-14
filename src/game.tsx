4db56dab900bbc2955594ae95156d1b9da1daccd
Gavin Haynes <haynes.gt@gmail.com>
2023-01-14 04:04:33 +0000

    feat: grame graph

--- src/game.tsx
+++ src/game.tsx
@@ -3,14 +3,68 @@ canvas.width = canvas.height = 512;
 document.body.append(canvas)
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
-class RenderNode {
+interface Vector2D {
+    x: number;
+    y: number;
 }
 
-class RenderGraph {
-    children: RenderGraph | RenderNode;
+interface Inputs {
+    l: Vector2D;
+    y: Vector2D;
 }
+
+interface StepData {
+    timeDeltaMs: number;
+    inputs: Inputs;
+}
+
+class GameObject {
+    parent: GameObject | undefined;
+
+    step(data: StepData) {}
+
+    render() {}
+}
+
+class GameGraph extends GameObject {
+    parent: GameObject | undefined;
+    children: GameObject[];
+    constructor() {
+        super();
+        this.children = []
+    }
+
+    add(obj: GameObject) {
+        this.children.push(obj);
+        obj.parent = this;
+    }
+
+    step(data: StepData) {
+        this.children.forEach(child => child.step(data));
+    }
+
+    render() {
+        this.children.forEach(child => child.render());
+    }
+}
+
+class PlayerObject extends GameObject {
+    constructor() {
+        super();
+    }
+
+    step(data: StepData) {
+    }
+
+    render() {
+    }
+}
+
+const objectGraph = new GameGraph();
+objectGraph.add(new PlayerObject());
 
 ctx.fillStyle = "green";
+ctx.transform
 ctx.fillRect(10, 10, 150, 100);
 
 let lastStepTimeMs: number = 0;
@@ -24,10 +78,13 @@ function step() {
     );
     lastStepTimeMs = nowMs;
 
-    let input;
+    let inputs;
     if (gamepad) {
-        input = { l: {x: gamepad.axes[0],y: gamepad.axes[1]}, r: {x: gamepad.axes[2],y: gamepad.axes[3]} }
+        inputs = { l: {x: gamepad.axes[0],y: gamepad.axes[1]}, r: {x: gamepad.axes[2],y: gamepad.axes[3]} }
     } else {
-        input = { l: { x: 0, y: 0 }, r: { x: 0, y: 0 } };
+        inputs = { l: { x: 0, y: 0 }, r: { x: 0, y: 0 } };
     }
+
+    objectGraph.step({timeDeltaMs: timeDeltaMs, input: inputs});
+    objectGraph.render();
 }
