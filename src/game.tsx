5f7f2391662be49ce85640fc8cbb7df4ba3ef54e
Gavin Haynes <haynes.gt@gmail.com>
2023-01-14 03:41:31 +0000

    feat: start game

---
+++ src/game.tsx
@@ -0,0 +1,44 @@
+const canvas = document.createElement("canvas");
+canvas.width = canvas.height = 512;
+document.body.append(canvas)
+const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+class RenderNode {
+}
+
+class RenderGraph {
+    children: RenderGraph | RenderNode;
+}
+
+ctx.fillStyle = "green";
+ctx.fillRect(10, 10, 150, 100);
+
+let lastStepTimeMs: number = 0;
+const minFps = 10;
+function step() {
+    const gamepad = navigator.getGamepads()[0];
+
+    const nowMs = new Date().getTime();
+    const timeDeltaMs = Math.min(
+        nowMs - lastStepTimeMs, 1000 / minFps
+    );
+    lastStepTimeMs = nowMs;
+
+    let gamepadInput;
+    if (gamepad) {
+        gamepadInput = {
+            l: {
+                x: gamepad.axes[0],
+                y: gamepad.axes[1],
+            },
+            r: {
+                x: gamepad.axes[2],
+                y: gamepad.axes[3],
+            }
+        }
+    } else {
+        gamepadInput = {
+            l: { x: 0, y: 0 }, r: { x: 0, y: 0 }
+        };
+    }
+}
